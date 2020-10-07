f013de9bfd4880364d514581b3acec4de320351a
Ernesto Cuadra Foy <46689746+ecuadrafoy@users.noreply.github.com>
2020-10-07 10:13:53 -0400

    migration to PostgreSQL

--- collator/settings.py
+++ collator/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'collator.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '4cent',
+        'USER': '4centadmin',
+        'PASSWORD': 'admin',    
     }
 }
 
