b00c7de5975a5cfe69307a70ddfb5dd26488d1f5
Ernesto Cuadra Foy <46689746+ecuadrafoy@users.noreply.github.com>
2020-10-17 17:50:03 -0400

    add login

--- collator/settings.py
+++ collator/settings.py
@@ -16,7 +16,7 @@ from django.contrib.messages import constants as messages
 from django.urls import reverse_lazy
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
