1f6ea3c8011a8bfa50429fa4708f308d428f42ac
Ernesto Cuadra Foy <46689746+ecuadrafoy@users.noreply.github.com>
2020-10-17 15:45:41 -0400

    applied some changes

--- collator/settings.py
+++ collator/settings.py
@@ -89,15 +89,15 @@ WSGI_APPLICATION = 'collator.wsgi.application'
 
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
-    }
+       'default': {
+             'ENGINE':'django.db.backends.postgresql_psycopg2',
+             'NAME': os.environ['RDS_DB_NAME'],
+             'USER': os.environ['RDS_USERNAME'],
+             'PASSWORD': os.environ['RDS_PASSWORD'],
+             'HOST': os.environ['RDS_HOSTNAME'],
+             'PORT': os.environ['RDS_PORT'],
+             }
+          }
 else:
     DATABASES = {
         'default': {
