ba0e61c65d04675d0b853fa49cf7c9fbe0346ab3
Ernesto Cuadra Foy <46689746+ecuadrafoy@users.noreply.github.com>
2020-10-17 16:33:52 -0400

    fixed createsu

--- logger/management/commands/createsu.py
+++ logger/management/commands/createsu.py
@@ -6,4 +6,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@admin.com", "B!utrm28)
+            User.objects.create_superuser("admin", "admin@admin.com", "B!utrm28")
