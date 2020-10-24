e1bd424f6749e356833a4be624be72517a9c5495
Ernesto Cuadra Foy <46689746+ecuadrafoy@users.noreply.github.com>
2020-10-24 04:51:52 -0400

    export Traffic as csv

--- logger/admin.py
+++ logger/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
+import csv, datetime
 
 from .models import Traffic, Source, Country, PIR, event_type, Organizations, Equipment, EventMatrix, Notes
 
@@ -14,6 +17,30 @@ admin.site.register(Equipment)
 #class SourceAdmin(admin.ModelAdmin):
 #	list_display = ['source_name']
 
+User = get_user_model()
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment;' 'filename={}.csv'.format(opts.verbose_name)
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    # Write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+    # Write data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+
+    return response
+
+export_to_csv.short_description = 'Export to CSV'  #short description
+
 
 @admin.register(Traffic)
 class TrafficAdmin(admin.ModelAdmin):
@@ -23,10 +50,12 @@ class TrafficAdmin(admin.ModelAdmin):
 	prepopulated_fields = {'traffic_slug': ('docname',)}
 	date_hierarchy = 'docdate'
 	ordering = ('status','docdate')
+	actions = [export_to_csv]
 
 @admin.register(EventMatrix)
 class EventAdmin(admin.ModelAdmin):
 	list_display = ('reference', 'event', 'location')
+	actions = [export_to_csv]
 
 @admin.register(Notes)
 class NotesAdmin(admin.ModelAdmin):
